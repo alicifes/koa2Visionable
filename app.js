a060936c6d992d9d4d707e7c9044885d872368ad
alicifes <635023574@qq.com>
2021-08-12 20:33:23 +0800

    websocket

--- app.js
+++ app.js
@@ -14,3 +14,21 @@ app.use(respDataMiddleware)
 
 //3.绑定端口号 8888
 app.listen(8888)
+
+const WebSocket = require('ws')
+//创建WebSocket的服务端对象,绑定的端口号是9998
+const wss =  new WebSocket.Server({
+    port:9998
+})
+//对客户端的连接事件进行监听
+//clien:代表的是客户端的连接socket对象
+wss.on('connection',client=>{
+    console.log("有客户端连接成功了...");
+    // 对客户端的连接对象进行message事件的监听
+    // msg:由客户端发给服务端的数据
+    client.on('message',msg=>{
+        console.log('客户端发送数据给服务端了:'+msg);
+        // 由服务端往客户端发送数据
+        client.send('hellow scoket from wss')
+    })
+})
